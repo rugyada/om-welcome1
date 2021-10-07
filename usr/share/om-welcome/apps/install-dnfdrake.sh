6ce38842806dcd574f70c870f14ab5cc0a0636ac
rugyada <46781309+rugyada@users.noreply.github.com>
2021-10-07 09:58:20 +0200

    Update install-dnfdrake.sh

--- usr/share/om-welcome/apps/install-dnfdrake.sh
+++ usr/share/om-welcome/apps/install-dnfdrake.sh
@@ -9,7 +9,7 @@ cd /tmp/dnfdrake-download
 dnf install -y dnf-utils python-dnf-plugin-versionlock
 enable-repo unsupported
 kdialog --title $"OpenMandriva" --msgbox $"Work in progress. Please wait... Step 2 of 3."
-dnf download gambas3-gb-dbus-3.15.2-6.x86_64 gambas3-gb-form-3.15.2-6.x86_64 gambas3-gb-gtk3-3.15.2-6.x86_64 gambas3-gb-gui-3.15.2-6.x86_64 gambas3-gb-image-3.15.2-6.x86_64 gambas3-gb-qt5-3.15.2-6.x86_64 gambas3-runtime-3.15.2-6.x86_64 dnfdrake.noarch
+dnf download gambas3-gb-dbus-3.15.2-6 gambas3-gb-form-3.15.2-6 gambas3-gb-gtk3-3.15.2-6 gambas3-gb-gui-3.15.2-6 gambas3-gb-image-3.15.2-6 gambas3-gb-qt5-3.15.2-6 gambas3-runtime-3.15.2-6 dnfdrake
 #
 kdialog --title $"OpenMandriva" --msgbox $"Work in progress. Step 3 of 3. Please wait until the process is done, it may take a while"
 rpm -ivh --nodeps *.rpm
